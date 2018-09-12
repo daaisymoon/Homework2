d8ca1effcb7dbff5b27760a17597382032c9bcc2
Student Generic Account <suzy.kirakosyan@gmail.com>
2018-09-12 16:02:41 +0400

    first commit

---
+++ homework.js
@@ -0,0 +1,29 @@
+const a = "Welcome to my website"
+
+alert(a);
+
+const members=5;
+const albums=6;
+const favSongs=2;
+
+console.log("Pink Floyd has" + members +"Members");
+console.log("There are" + albums + "famous albums");
+console.log("I have" + favSongs + "favourite songs");
+console.log(members+albums);
+console.log(albums*favSongs);
+
+   alert("This information is about Pink Floyd");
+   if(members===albums) {
+	  console.log("Yay, you will find this interesting");
+	  }
+	 else {
+	    console.log("Their music is amazing");	 
+		}
+		
+    if(albums!==favSongs){
+	console.log("Thanks for the attention");
+	}
+	else {
+	console.log("Good"+"bye");
+	}
+
